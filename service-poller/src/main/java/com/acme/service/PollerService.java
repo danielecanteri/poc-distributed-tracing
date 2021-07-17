395f6c628e55a7bbfa98de4c82e6275ff246b5c4
daniele.canteri <daniele.canteri@capgemini.com>
2021-07-17 09:48:43 +0200

    elastic

--- service-poller/src/main/java/com/acme/service/PollerService.java
+++ service-poller/src/main/java/com/acme/service/PollerService.java
@@ -1,5 +1,6 @@
 package com.acme.service;
 
+import io.opentelemetry.extension.annotations.WithSpan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,27 +15,33 @@ public class PollerService {
   private RestTemplate restTemplate;
 
   @Scheduled(initialDelay = 2000, fixedRate = 20000)
+  @WithSpan
   public void scheduled1() {
     restTemplate.getForEntity("http://localhost:8280/service-a", Map.class);
   }
 
   @Scheduled(initialDelay = 2000, fixedRate = 30000)
+  @WithSpan
   public void scheduled2() {
-    restTemplate.getForEntity("http://localhost:8280/service-a", Map.class);
+    restTemplate.getForEntity("http://localhost:8280/service-b", Map.class);
   }
 
   @Scheduled(initialDelay = 2000, fixedRate = 50000)
+  @WithSpan
   public void scheduled3() {
     restTemplate.getForEntity("http://localhost:8280/service-c", Map.class);
   }
 
   @Scheduled(initialDelay = 2000, fixedRate = 70000)
+  @WithSpan
   public void scheduled4() {
-    restTemplate.getForEntity("http://localhost:8380/db-1", Map.class);
+    restTemplate.getForEntity("http://localhost:8280/service-d", Map.class);
   }
 
   @Scheduled(initialDelay = 2000, fixedRate = 80000)
+  @WithSpan
   public void scheduled6() {
     restTemplate.getForEntity("http://localhost:8380/db-4", Map.class);
   }
+
 }
